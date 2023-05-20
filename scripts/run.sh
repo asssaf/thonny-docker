99c38e1025b93a01c74b58eae295c7d012fc4362
assaf <asssaf@localhost>
2023-05-20 10:05:51 -0700

    initial import

---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# if the device doesn't exist run will fail, so the device must be connected first
+# altenatively the container can be run in privileged mode
+
+# access to the X server needs to be enabled before starting the container (e.g. with "xhost +")
+
+: ${DEV:="/dev/ttyACM0"}
+: ${IMAGE:="asssaf/thonny"}
+
+docker run -it --rm \
+	-e DISPLAY=$DISPLAY \
+	-v /tmp/.X11-unix:/tmp/.X11-unix \
+	--device "$DEV" \
+	"$IMAGE" $*
